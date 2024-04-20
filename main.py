22d0132a3d0da79c98ba6d1fd334d5216d68257d
Monika <kmonika0987@gmail.com>
2024-04-20 17:02:25 +0100

    toggle pen

--- main.py
+++ main.py
@@ -21,30 +21,32 @@ pen_draw = True
 
 # Move Up
 def move_up():
-    canvas.create_line(coords[0], coords[1], coords[0], coords[1] + 10, fill="black", width=1)
+    canvas.create_line(coords[0], coords[1], coords[0], coords[1] + 10, fill=current_line_color, width=1)
     coords[1] += 10
 
 # Move Down
 def move_down():
-    canvas.create_line(coords[0], coords[1], coords[0], coords[1] - 10, fill="black", width=1)
+    canvas.create_line(coords[0], coords[1], coords[0], coords[1] - 10, fill=current_line_color, width=1)
     coords[1] -= 10
 
 # Move Left
 def move_left():
-    canvas.create_line(coords[0], coords[1], coords[0] - 10, coords[1], fill="black", width=1)
+    canvas.create_line(coords[0], coords[1], coords[0] - 10, coords[1], fill=current_line_color, width=1)
     coords[0] -= 10
 
 # Move Right
 def move_right():
-    canvas.create_line(coords[0], coords[1], coords[0] + 10, coords[1], fill="black", width=1)
+    canvas.create_line(coords[0], coords[1], coords[0] + 10, coords[1], fill=current_line_color, width=1)
     coords[0] += 10
 
 # Turn Left
 def turn_left():
+    # press the button followed by a number which is equal to the number of degrees it turns
     pass
 
 # Turn Right
 def turn_right():
+    # press the button followed by a number which is equal to the number of degrees it turns
     pass
 
 
@@ -60,6 +62,14 @@ def change_line_color():
     current_line_color = chosen_color[1]
 
 
+## PEN UP/DOWN
+
+# Toggle Pen
+def toggle_pen():
+    global pen_draw
+    pen_draw = not pen_draw
+
+
 ## BUTTONS
 
 # Movement Controls
@@ -72,5 +82,9 @@ tk.Button(window, text="↱", command=turn_left).grid(column=2, row=2)
 
 # Change Color
 tk.Button(window, text="Change Color", command=change_line_color).grid(column=2, row=3)
+
+# Pen Up/Down
+tk.Button(window, text="Pen Up/Down", command=toggle_pen).grid(column=1, row=3)
+
 
 window.mainloop()
